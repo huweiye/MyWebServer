e710ae4b7a6f5d84ed771a76500b2dac2636e118
huweiye <2920566283@qq.com>
2022-07-21 13:36:47 +0800

    add CMakeList

--- config.h
+++
@@ -1,9 +0,0 @@
-#ifndef _CONFIG_H_
-#define _CONFIG_H_
-#include<string>
-extern const int MAXLISTENQUEUE;
-extern const int MAXFDNUMS;
-extern const int MAXEVENTNUM;
-extern const std::string PATH;
-extern const int TIMER_TIME_OUT;
-#endif
